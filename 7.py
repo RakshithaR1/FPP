0ce52d45e1624aeba0245c63cba35b8c040f0b42
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2024-04-01 16:31:50 +0530

    test

--- 7.py
+++ 7.py
@@ -1,10 +1,9 @@
 dict = {'a':'b','c':'d','e':'f','g':'h'}
-cs=()
+
 arr=[]
 for key,value in dict.items():
-  cs=(key,value)
-  arr.append(cs)
+  arr.append(key+"="+value)
 print(arr)
-result = ";".join(["=".join(ele) for ele in arr])
+result = ";".join(arr)
 print(result)
                   
