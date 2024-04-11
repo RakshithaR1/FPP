6b74590fdfb24e72586b2b3a1814ead281f36120
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2024-04-11 16:21:54 +0530

    test

---
+++ iterator.py
@@ -0,0 +1,13 @@
+my_list = [1, 2, 3]
+
+# Obtaining an iterator for my_list
+iterator = iter(my_list)
+
+# Looping over the iterator
+while True:
+    try:
+        # Calling __next__() on the iterator
+        item = iterator.__next__()
+        print(item)  # Print the current item
+    except StopIteration:
+        break  # Exit the loop when StopIteration is raised
