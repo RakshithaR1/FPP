7a984633e5a8bc1d3d987753ccef09a2a95c19cd
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2024-04-12 14:18:47 +0530

    test

--- legb.py
+++ legb.py
@@ -16,3 +16,45 @@ def outer_function():
     inner_function()
 
 outer_function()
+
+# Define a decorator function
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()  # Call the original function
+        print("Something is happening after the function is called.")
+    return wrapper
+
+# Define a function to be decorated
+
+def say_hello():
+    print("Hello!")
+
+# Apply the decorator
+@my_decorator
+def decorated_say_hello():
+    print("Hello!")
+
+# Call the original function
+print("Calling original function:")
+say_hello()
+
+# Call the decorated function
+print("\nCalling decorated function:")
+decorated_say_hello()
+
+def leap_year(year):
+    
+    match year % 4, year % 100, year % 400:
+        case (0, 0, 0):
+            print("The year is a leap year.") 
+        case (0, 0, _):
+            print("The year is not a leap year.")  # Divisible by 4 and 100, but not 400 => Not a leap year
+        case (0, _, _):
+            print("The year is a leap year.")  # Divisible by 4, but not by 100 => Leap year
+        case (_, _, _):
+            print("The year is not a leap year.")  # Not divisible by 4 => Not a leap year
+
+y = int(input("Enter the year: "))
+leap_year(y)
+         
