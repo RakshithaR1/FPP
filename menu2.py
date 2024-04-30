ca513d1f893b35bf67d6769d36884968fe752428
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2024-04-30 11:16:54 +0530

    test

--- menu2.py
+++ menu2.py
@@ -1,10 +1,9 @@
 class menu:
   def __init__(self):
-    self.item = ()
+    self.item = {}
     
   def show(self):
-    menu_dict = {self.item[i] : self.item[i+1] for i in range(0,len(self.item),2)}
-    print(menu_dict)
+    print(self.item)
     
 m = menu()
 m + ("idly",10) + ("vada",20)
